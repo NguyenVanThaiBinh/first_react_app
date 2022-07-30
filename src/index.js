0040e172dbc1ace480251dbeb66ca52dd4339d87
Binh Hu <nguyenvanthaibinh2210@gmail.com>
2022-07-30 17:21:51 +0700

    update test file

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Test from './test';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+     <Test />
     <App />
+   
   </React.StrictMode>,
   document.getElementById('root')
 );
---
+++ src/test.js
@@ -0,0 +1,8 @@
+// @ts-nocheck
+
+function test(){
+return(
+         <h1>BinhHu</h1>
+)
+}
+export default test;
